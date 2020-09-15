509910b7e783989ebb0fd076b4db971714e7287b
Truc Krystosek <destructible88@gmail.com>
2020-09-15 10:57:08 -0500

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    //imageUrl: 'https://picsum.photos/200'
+  };
+  /*styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };*/
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <span /*style={{fontSize: 10}}*/ className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-small">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
