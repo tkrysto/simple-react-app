e5394b4251305b4b833c7065f10fa8c71117b6d2
Truc Krystosek <destructible88@gmail.com>
2020-09-15 11:32:26 -0500

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
     //imageUrl: 'https://picsum.photos/200'
   };
   /*styles = {
@@ -10,15 +11,35 @@ class Counter extends Component {
     fontWeight: "bold",
   };*/
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
-        <span /*style={{fontSize: 10}}*/ className="badge badge-primary m-2">
+        <span /*style={{fontSize: 10}}*/ className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-small">Increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
