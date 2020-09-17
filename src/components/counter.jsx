6953244290a61b24b0a3ecac8292cf66f909c0fa
Truc Krystosek <destructible88@gmail.com>
2020-09-17 10:35:06 -0500

    adding counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
     //imageUrl: 'https://picsum.photos/200'
   };
@@ -11,53 +11,53 @@ class Counter extends Component {
     fontWeight: "bold",
   };*/
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>no tags</p>;
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p>no tags</p>;
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //     return (
+  //       <ul>
+  //         {this.state.tags.map((tag) => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   }
   //   constructor() { //replaced by arrow function syntax as seen in the handleIncrement function below
   //       super();
   //       this.handleIncrement = this.handleIncrement.bind(this)
   //   }
   handleIncrement = (product) => {
     console.log("increment clicked", this);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
-        <span /*style={{fontSize: 10}}*/ className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button
-          onClick={() => this.handleIncrement(product)}
-          className="btn btn-secondary btn-small"
-        >
-          Increment
-        </button>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+        <div>
+          <span /*style={{fontSize: 10}}*/ className={this.getBadgeClasses()}>
+            {this.formatCount()}
+          </span>
+          <button
+            onClick={() => this.handleIncrement({ id: 1 })}
+            className="btn btn-secondary btn-small"
+          >
+            Increment
+          </button>
+        </div>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
